8a0c990679a9d27811058178b642b01341392464
Marc Stirner <marc.stirner@hotmail.de>
2026-02-21 12:00:05 +0100

    added

---
+++ src/components/Iframe.vue
@@ -0,0 +1,26 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    src: {
+      type: String,
+      default: "http://example.com"
+    }
+  },
+  methods: {
+    openInNewWindow() {
+      //window.open("/api/plugins/68aafde263362b527fcbdc8e/proxy/", "_blank");
+      // "width=800,height=600,menubar=no,toolbar=no,location=no,status=no,resizable=yes,scrollbars=yes"
+      window.open(this.src, "iframeWindow", "menubar=no,toolbar=no,location=no,status=no,resizable=yes,scrollbars=yes");
+    }
+  }
+});
+
+</script>
+
+<template>
+  <div class="w-100 h-100">
+    <iframe :src="src" class="w-100 h-100"></iframe>
+  </div>
+</template>
