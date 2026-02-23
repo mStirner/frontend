b1f6d890ebb0357e85f4b160de2addeb13a10640
Marc Stirner <marc.stirner@hotmail.de>
2026-02-23 11:23:49 +0100

    unecessary code removed

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,32 +15,7 @@ module.exports = (grunt) => {
     // Project configuration.
     grunt.initConfig({
         pkg,
-        env: {
-            options: {
-                //Shared Options Hash
-            },
-            prod: {
-                NODE_ENV: "production",
-            }
-        },
-        /*
-        compress: {
-            main: {
-                options: {
-                    archive: `frontend-v${pkg.version}.tgz`
-                },
-                files: [{
-                    expand: true,
-                    src: "**",
-                    cwd: "dist/"
-                }]
-            }
-        }
-        */
     });
-
-    grunt.loadNpmTasks("grunt-contrib-compress");
-    grunt.loadNpmTasks("grunt-env");
 
     grunt.registerTask("generate-about-json", () => {
 
